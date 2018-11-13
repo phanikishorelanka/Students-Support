ed8faebd8e600c1823628274a7df8e81201f30f0
root <root@buildserver1.int.ora>
2018-11-13 10:00:54 -0500

    updated

---
+++ scripts/setup_docker.sh
@@ -0,0 +1,34 @@
+echo " Removing any Old Docker Versions" 
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+
+yum -y install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum-config-manager --enable docker-ce-test
+yum -y install docker-ce
+
+echo "Docker Setup is complete" 
+
+yum list docker-ce --showduplicates | sort -r
+echo "Starting up Docker"
+systemctl start docker
+
+echo "Docker started successfully" 
+
+echo " CHecking Docker status..........."
+
+systemctl status docker 
+
+echo " setting docker to run after reboot"
+
+chkconfig docker on
+
+echo ################# Checking Docker Version ##############
+sleep 3
+docker version
+
+
+#setup virtual box 
+wget -N https://download.virtualbox.org/virtualbox/5.2.8/VirtualBox-5.2.8-121009-Linux_amd64.run
+chmod a+x VirtualBox-5.2.8-121009-Linux_amd64.run
+./VirtualBox-5.2.8-121009-Linux_amd64.run
---
+++ scripts/setup_k8s_minikube.sh
@@ -0,0 +1,14 @@
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+
+export MINIKUBE_WANTUPDATENOTIFICATION=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_HOME=$HOME
+export CHANGE_MINIKUBE_NONE_USER=true
+mkdir -p $HOME/.kube || true
+touch $HOME/.kube/config
+
+cp kubectl minikube /usr/local/bin
+export KUBECONFIG=$HOME/.kube/config
+minikube start --vm-driver=none
+minikube start
