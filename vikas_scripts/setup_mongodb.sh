a8c45c7a67f5be1f860ff2c0010e30659df61985
phanikishorelanka <phanikishorelanka@gmail.com>
2019-01-20 21:03:58 +0530

    Update setup_mongodb.sh

--- vikas_scripts/setup_mongodb.sh
+++ vikas_scripts/setup_mongodb.sh
@@ -2,7 +2,7 @@ cd /etc/yum.repos.d/
 wget https://raw.githubusercontent.com/phanikishorelanka/docker-k8s-pklanka/master/mongo.repo
 cp /etc/security/limits.conf /etc/security/limits.conf.orig
 echo mongod soft nofile 64000  >>/etc/security/limits.conf
-mongod hard nofile 64000 >>/etc/security/limits.conf
+echo mongod hard nofile 64000 >>/etc/security/limits.conf
 sysctl -p
 
 yum -y install mongodb-org
