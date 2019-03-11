a10c2c9b1d5ebc920909cba37ae31b6c221918bd
PHANI KISHORE LANKA <phanikishorelanka@PHANIs-iMac.local>
2019-03-11 22:35:19 +0530

    setup

---
+++ vikas_scripts/setup_jenkins_aws.sh
@@ -0,0 +1,12 @@
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum -y install java
+sudo yum -y install java-1.8.0-openjdk-devel
+
+sudo yum -y install jenkins
+
+sudo systemctl status jenkins.service
+sudo systemctl enable jenkins
+chkconfig jenkins on 
